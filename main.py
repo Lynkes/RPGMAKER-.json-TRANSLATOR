db2df6421b3f700a795142788963a3ea074dade3
Pedro Guilherme Gabriel Maurer <48015282+Lynkes@users.noreply.github.com>
2024-07-03 09:03:24 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,57 @@
 import json
 import os
 import tkinter as tk
-from tkinter import ttk, filedialog
+from tkinter import filedialog
 from colorama import Fore, Style
 from googletrans import LANGUAGES, Translator
 from concurrent.futures import ThreadPoolExecutor
 
-# Definindo as mensagens de entrada (corrigindo o JSON)
+# Defining input messages (correcting JSON)
 
-def processar_linha(linha, target_language):
-    translated_text = translate_text(linha, target_language)
-    return translated_text
+def process_element(element, target_language: str):
+    if isinstance(element, str):
+        return translate_text(element, target_language)
+    elif isinstance(element, dict):
+        return {key: process_element(value, target_language) for key, value in element.items()}
+    elif isinstance(element, list):
+        return [process_element(value, target_language) for value in element]
+    else:
+        return element
 
-def ler_arquivo_json(nome_arquivo, target_languages, output_dir):
-    resultados = {lang: {} for lang in target_languages}
+def read_json_file(file_name: str, target_languages: list[str], output_dir: str):
     try:
-        with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
-            dados = json.load(arquivo)
+        with open(file_name, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            if isinstance(data, dict):
+                results = {lang: {} for lang in target_languages}
+            elif isinstance(data, list):
+                results = {lang: [] for lang in target_languages}
             for lang in target_languages:
                 with ThreadPoolExecutor(max_workers=10) as executor:
-                    future_to_key = {executor.submit(processar_linha, valor, lang): chave for chave, valor in dados.items()}
-                    for future in future_to_key:
-                        chave = future_to_key[future]
-                        resultado = future.result()
-                        resultados[lang][chave] = resultado.strip()  # Adiciona o resultado ao dicionário de resultados
+                    if isinstance(data, dict):
+                        future_to_key = {executor.submit(process_element, value, lang): key for key, value in data.items()}
+                        for future in future_to_key:
+                            key = future_to_key[future]
+                            result = future.result()
+                            results[lang][key] = result
+                    elif isinstance(data, list):
+                        futures = [executor.submit(process_element, item, lang) for item in data]
+                        results[lang] = [future.result() for future in futures] # type: ignore
 
-        # Criar diretório de saída se não existir
+        # Create output directory if doesn't exist
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
-        # Salvar os resultados em novos arquivos JSON para cada linguagem
+        # Save results to new JSON files for each language
         for lang in target_languages:
-            with open(os.path.join(output_dir, f'Translated_{lang}.json'), 'w', encoding='utf-8') as arquivo_saida:
-                json.dump(resultados[lang], arquivo_saida, indent=2, ensure_ascii=False)  # Garante que os caracteres acentuados sejam mantidos
+            lang_dir = os.path.join(output_dir, lang)
+            if not os.path.exists(lang_dir):
+                os.makedirs(lang_dir)
+            with open(os.path.join(lang_dir, os.path.basename(file_name)), 'w', encoding='utf-8') as output_file:
+                json.dump(results[lang], output_file, indent=2, ensure_ascii=False)  # Ensures that accented characters are maintained
 
     except FileNotFoundError:
-        print(f"Arquivo '{nome_arquivo}' não encontrado.")
+        print(f"File '{file_name}' not found.")
 
 def select_folder():
     folder_path = filedialog.askdirectory(title="Select Folder")
@@ -48,17 +64,17 @@ def select_folder():
         print(Style.BRIGHT + Fore.RED,)
         print("No folder selected.")
 
-def translate_text(text, target_language):
+def translate_text(text: str, target_language: str):
     try:
         translator = Translator()
-        translation = translator.translate(text, dest=target_language)
+        translation = translator.translate(text, dest=target_language, src='auto')
         if translation:
-            return translation.text
+            return translation.text # type: ignore
         else:
-            print(Style.BRIGHT + Fore.RED + "Translation failed. Empty response received.")
+            print(Style.BRIGHT + Fore.RED + "Translation failed. Empty response received." + Fore.RESET)
             return ""
     except Exception as e:
-        print(Style.BRIGHT + Fore.RED + f"Translation error: {e}")
+        print(Style.BRIGHT + Fore.RED + f"Translation error: {e}" + Fore.RESET)
         return ""
 
 def ui():
@@ -76,25 +92,29 @@ def ui():
         listbox.insert(tk.END, name)
     listbox.pack(padx=10, pady=10)
 
-    translate_button = tk.Button(root, text="Translate", command=lambda: process_files_and_translate(select_folder(), [language_mapping[listbox.get(i)] for i in listbox.curselection()]))
+    translate_button = tk.Button(root, text="Translate", command=lambda: process_files_and_translate(
+        select_folder(),
+        [language_mapping[listbox.get(i)] for i in listbox.curselection()]))
     translate_button.pack(pady=10)
     
     root.mainloop()
 
-def process_files_and_translate(folder_path, target_languages):
-    if folder_path:
-        files = os.listdir(folder_path)
-        json_files = [file for file in files if file.lower().endswith('.json')]
-        output_dir = os.path.join(folder_path, 'Translation')
-        for json_file in json_files:
-            json_file_path = os.path.join(folder_path, json_file)
-            print(Style.BRIGHT + Fore.MAGENTA,)
-            print(f"Processing file: {json_file_path}")
-            ler_arquivo_json(json_file_path, target_languages, output_dir)
-        
-        print(Style.BRIGHT + Fore.GREEN,)
-        print("ALL .json files Generated:")
-        print(Style.BRIGHT + Fore.RESET,)
+def process_files_and_translate(folder_path: str | None, target_languages: list[str]):
+    if not folder_path:
+        return
+    folder_path = os.path.normpath(folder_path)
+    files = os.listdir(folder_path)
+    json_files = [file for file in files if file.lower().endswith('.json')]
+    output_dir = os.path.join(folder_path, 'Translation')
+    for json_file in json_files:
+        json_file_path = os.path.join(folder_path, json_file)
+        print(Style.BRIGHT + Fore.MAGENTA,)
+        print(f"Processing file: {json_file_path}")
+        read_json_file(json_file_path, target_languages, output_dir)
+    
+    print(Style.BRIGHT + Fore.GREEN,)
+    print("ALL .json files Generated:")
+    print(Style.BRIGHT + Fore.RESET,)
 
 # Launch the UI
 ui()
